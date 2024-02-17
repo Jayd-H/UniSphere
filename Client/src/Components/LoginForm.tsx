34cafcd685fc7bf85646a9d964397b5c00f50d7c
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-17 03:10:50 +0000

    Made the login form look prettier

--- Client/src/Components/LoginForm.tsx
+++ Client/src/Components/LoginForm.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { UserIcon, LockClosedIcon } from '@heroicons/react/24/outline';
 import uniSphereLogo from '../assets/UniSphereLogo.svg';
 
 const LoginForm = () => {
@@ -8,60 +9,56 @@ const LoginForm = () => {
   // Handle form submit
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // Here you would handle the login logic
     console.log('Login submitted for:', username, password);
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-luni-light-blue"> 
-    <div className="bg-luni-white p-8 rounded-lg shadow-lg max-w-sm w-full"> 
-      <div className="text-center mb-6">
-      <img src={uniSphereLogo} alt="UniSphere Logo" className="h-16 w-16" />
-      </div>
-      <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">
-              Name
-            </label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Enter your username..."
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Enter your password..."
-              required
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <button
-              type="submit"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
-              Login
-            </button>
-            <button
-              type="button"
-              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-            >
-              Register
-            </button>
-          </div>
-        </form>
+    <div className="flex justify-center items-center h-screen bg-luni-light-blue font-arimo">
+    <div className="bg-luni-white shadow-lg rounded-lg max-w-sm mx-auto p-6">
+        <div className="flex flex-col items-center p-8">
+          <img src={uniSphereLogo} alt="UniSphere Logo" className="h-15 w-48" />
+          <h1 className="text-xl font-bold text-luni-grey mb-6">Login</h1>
+          <form onSubmit={handleSubmit} className="space-y-8">
+            <div className="relative">
+              <UserIcon className="h-5 w-5 text-luni-grey absolute left-0 inset-y-0 flex items-center pl-2 mt-2.5" />
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="py-2 pl-10 block w-full bg-transparent border-0 border-b-2 border-gray-300 focus:border-luni-blue outline-none font-montserrat placeholder-luni-grey"
+                placeholder="Enter username..."
+                required
+              />
+            </div>
+            <div className="relative">
+              <LockClosedIcon className="h-5 w-5 text-luni-grey absolute left-0 inset-y-0 flex items-center pl-2 mt-2.5" />
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="py-2 pl-10 block w-full bg-transparent border-0 border-b-2 border-gray-300 focus:border-luni-blue outline-none font-montserrat placeholder-luni-grey"
+                placeholder="Enter password..."
+                required
+              />
+            </div>
+            <div className="flex items-center justify-center">
+              <button
+                type="submit"
+                className="bg-luni-blue hover:bg-luni-dark-blue text-white font-bold py-1 px-4 rounded-xl focus:outline-none focus:shadow-outline"
+              >
+                Login
+              </button>
+              <button
+                type="button"
+                className="ml-4 inline-block align-baseline font-bold text-luni-blue hover:text-luni-dark-blue"
+              >
+                Register
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
