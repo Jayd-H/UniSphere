ba02179cd5f1feee696897d14cc8847bc6e7a86b
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-17 14:52:05 +0000

    Added fade in animation and preventing of usernames and passwords > 32

--- Client/src/Components/LoginForm.tsx
+++ Client/src/Components/LoginForm.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { UserIcon, LockClosedIcon } from '@heroicons/react/24/outline';
 import uniSphereLogo from '../assets/UniSphereLogo.svg';
+import { motion } from 'framer-motion';
 
 const LoginForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [isOverLimit, setIsOverLimit] = useState(false);
 
   // Handle form submit
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -12,9 +14,27 @@ const LoginForm = () => {
     console.log('Login submitted for:', username, password);
   };
 
+  // Function to handle username change and check the length
+  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputLength = e.target.value.length;
+    setUsername(e.target.value);
+    setIsOverLimit(inputLength > 32 || password.length > 32);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputLength = e.target.value.length;
+    setPassword(e.target.value);
+    setIsOverLimit(username.length > 32 || inputLength > 32);
+  };
+
   return (
-    <div className="flex justify-center items-center h-screen glowing-background font-arimo"> 
-    <div className="frosted-glass w-full max-w-xs p-8 rounded-lg shadow-lg">
+    <div className="flex justify-center items-center h-screen glowing-background font-arimo">
+      <motion.div
+        className="frosted-glass w-full max-w-xs p-8 rounded-lg shadow-lg"
+        initial={{ opacity: 0, y: 300 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ type: 'spring', stiffness: 120, damping: 25 }}
+      >
         <div className="flex flex-col items-center p-8">
           <img src={uniSphereLogo} alt="UniSphere Logo" className="h-15 w-48" />
           <h1 className="text-xl font-bold text-luni-grey mb-6">Login</h1>
@@ -25,7 +45,7 @@ const LoginForm = () => {
                 type="text"
                 id="username"
                 value={username}
-                onChange={(e) => setUsername(e.target.value)}
+                onChange={handleUsernameChange}
                 className="py-2 pl-10 block w-full bg-transparent border-0 border-b-2 border-gray-300 focus:border-luni-blue outline-none font-montserrat placeholder-luni-grey"
                 placeholder="Enter username..."
                 required
@@ -37,7 +57,7 @@ const LoginForm = () => {
                 type="password"
                 id="password"
                 value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={handlePasswordChange}
                 className="py-2 pl-10 block w-full bg-transparent border-0 border-b-2 border-gray-300 focus:border-luni-blue outline-none font-montserrat placeholder-luni-grey"
                 placeholder="Enter password..."
                 required
@@ -46,20 +66,32 @@ const LoginForm = () => {
             <div className="flex items-center justify-center">
               <button
                 type="submit"
-                className="bg-luni-blue hover:bg-luni-dark-blue text-white font-bold py-1 px-4 rounded-xl focus:outline-none focus:shadow-outline"
+                className={`bg-luni-blue hover:bg-luni-dark-blue text-white font-bold py-1 px-4 rounded-xl focus:outline-none focus:shadow-outline ${
+                  isOverLimit ? 'bg-gray-500 hover:bg-gray-500 cursor-not-allowed' : ''
+                }`}
+                disabled={isOverLimit}
               >
                 Login
               </button>
               <button
                 type="button"
-                className="ml-4 inline-block align-baseline font-bold text-luni-blue hover:text-luni-dark-blue"
+                className={`ml-4 inline-block align-baseline font-bold text-sm text-luni-blue hover:text-luni-dark-blue ${
+                  isOverLimit ? 'text-gray-500 cursor-not-allowed' : ''
+                }`}
+                disabled={isOverLimit}
+                // register button logic
               >
                 Register
               </button>
             </div>
+            {isOverLimit && (
+              <div className="text-red-500 text-sm">
+                Username and password must be less than 32 characters long.
+              </div>
+            )}
           </form>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
