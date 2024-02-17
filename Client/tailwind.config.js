0bad4c09dfa739865bbee13c0005238af89578fe
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-17 03:10:00 +0000

    Changed to a slightly lighter white

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
     extend: {
       colors: { 
         'luni-blue': '#60879A',
-        'luni-light-blue': '#787878',
-        'luni-white': '#EAEAEA',
+        'luni-light-blue': '#D6E2EC',
+        'luni-white': '#ededed',
         'luni-black': '#2E2E2E',
         'luni-grey': '#787878',
       },
