d1081ae6b48c05ea02fba23b98ecb41ce4debcca
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-17 01:24:41 +0000

    Imported custom colours into tailwind

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,9 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: { 
+        'luni-blue': '#60879A',
+        'luni-light-blue': '#787878',
+        'luni-white': '#EAEAEA',
+        'luni-black': '#2E2E2E',
+        'luni-grey': '#787878',
+      },
+
+    },
   },
   plugins: [],
-}
+
+};
 
