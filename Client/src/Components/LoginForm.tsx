8e7379f46f8693e81a234af3dd9b29979833961c
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-19 15:16:23 +0000

    Added simple state for adding display name on register

--- Client/src/Components/LoginForm.tsx
+++ Client/src/Components/LoginForm.tsx
@@ -1,17 +1,25 @@
-import React, { useState } from 'react';
-import { UserIcon, LockClosedIcon } from '@heroicons/react/24/outline';
-import uniSphereLogo from '../assets/UniSphereLogo.svg';
-import { motion } from 'framer-motion';
+import React, { useState } from "react";
+import { UserIcon, LockClosedIcon } from "@heroicons/react/24/outline";
+import uniSphereLogo from "../assets/UniSphereLogo.svg";
+import { motion } from "framer-motion";
 
 const LoginForm = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [isOverLimit, setIsOverLimit] = useState(false);
+  const [isRegistering, setIsRegistering] = useState(false);
 
-  // Handle form submit
+  // Handle form submit for login
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log('Login submitted for:', username, password);
+    console.log("Login submitted for:", username, password);
+  };
+
+  // Handle form submit for registration (display name creation)
+  const handleRegisterSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Here you would handle the display name submission logic
+    console.log("Display name submitted");
   };
 
   // Function to handle username change and check the length
@@ -27,73 +35,114 @@ const LoginForm = () => {
     setIsOverLimit(username.length > 32 || inputLength > 32);
   };
 
-  return (
-    <div className="flex justify-center items-center h-screen glowing-background font-arimo">
-      <motion.div
-        className="frosted-glass w-full max-w-xs p-8 rounded-lg shadow-lg"
-        initial={{ opacity: 0, y: 300 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ type: 'spring', stiffness: 120, damping: 25 }}
-      >
-        <div className="flex flex-col items-center p-8">
-          <img src={uniSphereLogo} alt="UniSphere Logo" className="h-15 w-48" />
-          <h1 className="text-xl font-bold text-luni-grey mb-6">Login</h1>
-          <form onSubmit={handleSubmit} className="space-y-8">
-            <div className="relative">
-              <UserIcon className="h-5 w-5 text-luni-grey absolute left-0 inset-y-0 flex items-center pl-2 mt-2.5" />
-              <input
-                type="text"
-                id="username"
-                value={username}
-                onChange={handleUsernameChange}
-                className="py-2 pl-10 block w-full bg-transparent border-0 border-b-2 border-gray-300 focus:border-luni-blue outline-none font-montserrat placeholder-luni-grey"
-                placeholder="Enter username..."
-                required
-              />
-            </div>
-            <div className="relative">
-              <LockClosedIcon className="h-5 w-5 text-luni-grey absolute left-0 inset-y-0 flex items-center pl-2 mt-2.5" />
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={handlePasswordChange}
-                className="py-2 pl-10 block w-full bg-transparent border-0 border-b-2 border-gray-300 focus:border-luni-blue outline-none font-montserrat placeholder-luni-grey"
-                placeholder="Enter password..."
-                required
-              />
-            </div>
-            <div className="flex flex-col items-center justify-center mt-8">
-  <button
-    type="submit"
-    className={`bg-luni-blue hover:bg-luni-dark-blue text-white font-bold py-2 px-6 rounded-xl focus:outline-none focus:shadow-outline w-36 ${
-      isOverLimit ? 'bg-gray-500 hover:bg-gray-500 cursor-not-allowed' : ''
-    }`}
-    disabled={isOverLimit}
-  >
-    Login
-  </button>
-  <button
-    type="button"
-    className={`mt-4 hover:text-luni-dark-blue text-luni-blue font-bold py-2 px-6 rounded-xl focus:outline-none focus:shadow-outline w-36 ${
-      isOverLimit ? 'text-gray-500 cursor-not-allowed' : ''
-    }`}
-    disabled={isOverLimit}
-    // register button logic
-  >
-    Register
-  </button>
-</div>
-            {isOverLimit && (
-              <div className="text-red-500 text-sm">
-                Username and password must be less than 32 characters long.
+  // Conditional rendering based on whether the user is registering or logging in
+  if (isRegistering) {
+    return (
+      <div className="flex justify-center items-center h-screen glowing-background font-arimo">
+        <motion.div
+          className="frosted-glass w-full max-w-xs p-8 rounded-lg shadow-lg"
+          initial={{ opacity: 0, y: 300 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ type: "spring", stiffness: 120, damping: 25 }}
+        >
+          <div className="flex flex-col items-center p-8">
+            <h1 className="text-xl font-bold text-luni-grey mb-6">
+              Create Display Name
+            </h1>
+            <form onSubmit={handleRegisterSubmit} className="space-y-8">
+              <div className="relative">
+                <input
+                  type="text"
+                  id="displayName"
+                  className="py-2 pl-10 block w-full bg-transparent border-0 border-b-2 border-gray-300 focus:border-luni-blue outline-none font-montserrat placeholder-luni-grey"
+                  placeholder="Enter display name..."
+                  required
+                />
               </div>
-            )}
-          </form>
-        </div>
-      </motion.div>
-    </div>
-  );
+              <button
+                type="submit"
+                className="bg-luni-blue hover:bg-luni-dark-blue text-white font-bold py-2 px-6 rounded-xl focus:outline-none focus:shadow-outline w-36"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </motion.div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex justify-center items-center h-screen glowing-background font-arimo">
+        <motion.div
+          className="frosted-glass w-full max-w-xs p-8 rounded-lg shadow-lg"
+          initial={{ opacity: 0, y: 300 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ type: "spring", stiffness: 120, damping: 25 }}
+        >
+          <div className="flex flex-col items-center p-8">
+            <img
+              src={uniSphereLogo}
+              alt="UniSphere Logo"
+              className="h-15 w-48"
+            />
+            <h1 className="text-xl font-bold text-luni-grey mb-6">Login</h1>
+            <form onSubmit={handleSubmit} className="space-y-8">
+              <div className="relative">
+                <UserIcon className="h-5 w-5 text-luni-grey absolute left-0 inset-y-0 flex items-center pl-2 mt-2.5" />
+                <input
+                  type="text"
+                  id="username"
+                  value={username}
+                  onChange={handleUsernameChange}
+                  className="py-2 pl-10 block w-full bg-transparent border-0 border-b-2 border-gray-300 focus:border-luni-blue outline-none font-montserrat placeholder-luni-grey"
+                  placeholder="Enter username..."
+                  required
+                />
+              </div>
+              <div className="relative">
+                <LockClosedIcon className="h-5 w-5 text-luni-grey absolute left-0 inset-y-0 flex items-center pl-2 mt-2.5" />
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  className="py-2 pl-10 block w-full bg-transparent border-0 border-b-2 border-gray-300 focus:border-luni-blue outline-none font-montserrat placeholder-luni-grey"
+                  placeholder="Enter password..."
+                  required
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center mt-8">
+                <button
+                  type="submit"
+                  className={`bg-luni-blue hover:bg-luni-dark-blue text-white font-bold py-2 px-6 rounded-xl focus:outline-none focus:shadow-outline w-36 ${
+                    isOverLimit
+                      ? "bg-gray-500 hover:bg-gray-500 cursor-not-allowed"
+                      : ""
+                  }`}
+                  disabled={isOverLimit}
+                >
+                  Login
+                </button>
+                <button
+                  type="button"
+                  className="mt-4 hover:text-luni-dark-blue text-luni-blue font-bold py-2 px-6 rounded-xl focus:outline-none focus:shadow-outline w-36"
+                  disabled={isOverLimit}
+                  onClick={() => setIsRegistering(true)}
+                >
+                  Register
+                </button>
+              </div>
+              {isOverLimit && (
+                <div className="text-red-500 text-sm">
+                  Username and password must be less than 32 characters long.
+                </div>
+              )}
+            </form>
+          </div>
+        </motion.div>
+      </div>
+    );
+  }
 };
 
 export default LoginForm;
