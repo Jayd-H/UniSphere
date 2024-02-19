06fa0cca5c8cd7f5bdac5e07b226853f5441efc7
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-19 01:08:49 +0000

    Added more mock data

--- Client/src/Components/Feed.tsx
+++ Client/src/Components/Feed.tsx
@@ -31,6 +31,33 @@ const mockPosts = [
         }
       ]
     },
+    {
+        username: 'Jayd Holdsworth',
+        societyName: 'Pokémon Society',
+        timestamp: new Date('2023-01-07T14:32:00').toISOString(),
+        content: 'Hey guys, super excited for the Pokémon fan meetup in the library today! My favourite Pokémon are Lopunny, Gardevoir, and Gothita. Hope everyone remembers to bring their best switch games. Can anyone confirm if snacks and drinks are available? When I win, I tend to get hungry!',
+        likesCount: 5,
+        replies: [
+          {
+            username: 'Ash Ketchum',
+            content: 'I’ll be there with my Pikachu!',
+            timestamp: new Date('2023-01-07T15:00:00').toISOString(),
+            likesCount: 3,
+          },
+          {
+            username: 'Misty Waterflower',
+            content: 'Can’t wait to meet everyone! I’ll bring some snacks and drinks for everyone.  See you all there! if you need anything else, let me know!',
+            timestamp: new Date('2023-01-07T15:15:00').toISOString(),
+            likesCount: 2,
+          },
+          {
+            username: 'Misty Waterflower',
+            content: 'This is going to be so much fun! I’ll bring some snacks and drinks for everyone.  See you all there!',
+            timestamp: new Date('2023-01-07T15:15:00').toISOString(),
+            likesCount: 0,
+          }
+        ]
+      },
 ]
 
 const Feed: React.FC = () => {
