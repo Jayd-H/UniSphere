cd74f14a9ee6ed681eaee669f4672060091ed827
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-19 15:44:59 +0000

    Improved styling

--- Client/src/Components/LoginForm.tsx
+++ Client/src/Components/LoginForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { UserIcon, LockClosedIcon } from "@heroicons/react/24/outline";
+import { UserIcon, LockClosedIcon, ArrowLeftIcon } from "@heroicons/react/24/outline";
 import uniSphereLogo from "../assets/UniSphereLogo.svg";
 import { motion } from "framer-motion";
 
@@ -40,31 +40,40 @@ const LoginForm = () => {
     return (
       <div className="flex justify-center items-center h-screen glowing-background font-arimo">
         <motion.div
-          className="frosted-glass w-full max-w-xs p-8 rounded-lg shadow-lg"
+          className="frosted-glass w-full max-w-xs p-4 rounded-lg shadow-lg relative"
           initial={{ opacity: 0, y: 300 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ type: "spring", stiffness: 120, damping: 25 }}
         >
-          <div className="flex flex-col items-center p-8">
-            <h1 className="text-xl font-bold text-luni-grey mb-6">
-              Create Display Name
-            </h1>
-            <form onSubmit={handleRegisterSubmit} className="space-y-8">
-              <div className="relative">
+          {/* Back button */}
+          <button
+            className="absolute top-2 left-2 text-luni-grey"
+            onClick={() => setIsRegistering(false)}
+          >
+            <ArrowLeftIcon className="h-5 w-5 mt-2 ml-2" />
+          </button>
+          
+          <div className="flex flex-col items-center p-8 w-full"> 
+            <h1 className="text-xl font-bold text-luni-grey mb-8 text-center w-full">Create Display Name</h1> 
+            <form onSubmit={handleRegisterSubmit} className="space-y-8 w-full"> 
+              <div className="relative w-full"> 
+                <UserIcon className="h-5 w-5 text-luni-grey absolute left-2 inset-y-0 flex items-center mt-2.5" />
                 <input
                   type="text"
                   id="displayName"
-                  className="py-2 pl-10 block w-full bg-transparent border-0 border-b-2 border-gray-300 focus:border-luni-blue outline-none font-montserrat placeholder-luni-grey"
+                  className="py-2 pl-10 pr-3 block w-full bg-transparent border-0 border-b-2 border-gray-300 focus:border-luni-blue outline-none font-montserrat placeholder-luni-grey"
                   placeholder="Enter display name..."
                   required
                 />
               </div>
-              <button
-                type="submit"
-                className="bg-luni-blue hover:bg-luni-dark-blue text-white font-bold py-2 px-6 rounded-xl focus:outline-none focus:shadow-outline w-36"
-              >
-                Submit
-              </button>
+              <div className="flex justify-center w-full">
+                <button
+                  type="submit"
+                  className="bg-luni-blue hover:bg-luni-dark-blue text-white font-bold py-2 px-6 rounded-xl focus:outline-none focus:shadow-outline"
+                >
+                  Submit
+                </button>
+              </div>
             </form>
           </div>
         </motion.div>
@@ -88,7 +97,7 @@ const LoginForm = () => {
             <h1 className="text-xl font-bold text-luni-grey mb-6">Login</h1>
             <form onSubmit={handleSubmit} className="space-y-8">
               <div className="relative">
-                <UserIcon className="h-5 w-5 text-luni-grey absolute left-0 inset-y-0 flex items-center pl-2 mt-2.5" />
+                <UserIcon className="h-5 w-5 text-luni-grey absolute left-2 inset-y-0 flex items-center mt-2.5" />
                 <input
                   type="text"
                   id="username"
@@ -100,7 +109,7 @@ const LoginForm = () => {
                 />
               </div>
               <div className="relative">
-                <LockClosedIcon className="h-5 w-5 text-luni-grey absolute left-0 inset-y-0 flex items-center pl-2 mt-2.5" />
+                <LockClosedIcon className="h-5 w-5 text-luni-grey absolute left-2 inset-y-0 flex items-center mt-2.5" />
                 <input
                   type="password"
                   id="password"
