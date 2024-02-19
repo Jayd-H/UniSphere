d3ab432f89140ece85231461e148369f1e627013
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-19 01:27:46 +0000

    Removed unnecessary comments

--- Client/src/Pages/HomePage.tsx
+++ Client/src/Pages/HomePage.tsx
@@ -1,11 +1,9 @@
 // HomePage.tsx
 import React from 'react';
-import Feed from '../Components/Feed'; // Adjust the path as necessary
+import Feed from '../Components/Feed';
 
 const HomePage: React.FC = () => {
   return (
-    // Apply the bg-luni-light-blue class to the top-level div
-    // Also ensure it stretches to the full height by using h-screen
     <div>
       <Feed />
     </div>
