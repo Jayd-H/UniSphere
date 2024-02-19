97f69dbff166af4ab524a39677ad98ab69bb6bc4
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-19 00:40:02 +0000

    Removed unnecessary comments

--- Client/src/Pages/HomePage.tsx
+++ Client/src/Pages/HomePage.tsx
@@ -1,6 +1,6 @@
 // HomePage.tsx
 import React from 'react';
-import Feed from '../Components/Feed'; // Adjust the path as necessary
+import Feed from '../Components/Feed';
 
 const HomePage: React.FC = () => {
   return (
