e111640529164c57e58a9ac02e2652086933ae87
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-20 04:54:34 +0000

    Added field requirement to error handling and fixed rendering of error messages

--- Client/src/Components/LoginForm.tsx
+++ Client/src/Components/LoginForm.tsx
@@ -36,12 +36,17 @@ const LoginForm = () => {
   };
 
   // This function sets the error message based on the input validation
-  const setValidationMessage = () => {
-    if (!validateInput(username, 5, 32, false)) {
+  const setValidationMessage = (
+    field: "username" | "password" | "displayName"
+  ) => {
+    if (field === "username" && !validateInput(username, 5, 32, false)) {
       setErrorMessage("Username must be 5-32 characters long without spaces.");
-    } else if (!validateInput(password, 5, 32, false)) {
+    } else if (field === "password" && !validateInput(password, 5, 32, false)) {
       setErrorMessage("Password must be 5-32 characters long without spaces.");
-    } else if (isRegistering && !validateInput(displayName, 5, 64, true)) {
+    } else if (
+      field === "displayName" &&
+      !validateInput(displayName, 5, 64, true)
+    ) {
       setErrorMessage("Display name must be 5-64 characters long.");
     } else {
       setErrorMessage("");
@@ -51,21 +56,20 @@ const LoginForm = () => {
   // These functions handle the input changes and set the error message
   const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUsername(e.target.value);
-    setValidationMessage();
+    setValidationMessage("username");
   };
 
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
-    setValidationMessage();
+    setValidationMessage("password");
   };
 
   const handleDisplayNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setDisplayName(e.target.value);
     if (isRegistering) {
-      setValidationMessage();
+      setValidationMessage("displayName");
     }
   };
-
   // This function checks if the form is valid based on the error message
   const validateForm = () => {
     if (errorMessage === "") {
@@ -89,7 +93,7 @@ const LoginForm = () => {
     }
   };
 
-  // This function checks if the form should be disabled based on the input validation 
+  // This function checks if the form should be disabled based on the input validation
   const shouldDisableForm = () => {
     if (
       !validateInput(username, 5, 32, false) ||
@@ -104,7 +108,6 @@ const LoginForm = () => {
   };
 
   return (
-    // Registration form
     <div className="flex justify-center items-center h-screen glowing-background font-arimo">
       <motion.div
         className="frosted-glass w-full max-w-xs p-4 rounded-lg shadow-lg relative"
@@ -133,10 +136,17 @@ const LoginForm = () => {
                   id="displayName"
                   value={displayName}
                   onChange={handleDisplayNameChange}
-                  className="py-2 pl-10 pr-3 block w-full bg-transparent border-0 border-b-2 border-gray-300 focus:border-luni-blue outline-none font-montserrat placeholder-luni-grey"
+                  className={`py-2 pl-10 block w-full bg-transparent border-0 border-b-2 outline-none font-montserrat placeholder-luni-grey${
+                    errorMessage.includes("Display name")
+                      ? "border-red-500"
+                      : "border-gray-600"
+                  } focus:border-luni-blue outline-none font-montserrat placeholder-luni-grey`}
                   placeholder="Enter display name..."
                   required
                 />
+                {errorMessage.includes("Display name") && (
+                  <p className="text-red-500 text-xs italic">{errorMessage}</p>
+                )}
                 <button
                   type="submit"
                   disabled={shouldDisableForm()}
@@ -150,7 +160,6 @@ const LoginForm = () => {
             </div>
           </>
         ) : (
-          // Login form
           <>
             <div className="flex flex-col items-center p-8">
               <img
@@ -187,24 +196,25 @@ const LoginForm = () => {
                 <div className="flex flex-col items-center justify-center mt-8">
                   <button
                     type="submit"
-                    className={`bg-luni-blue hover:bg-luni-dark-blue text-white font-bold py-2 px-6 rounded-xl focus:outline-none focus:shadow-outline w-36 ${
-                      shouldDisableForm()
-                        ? "bg-gray-500 hover:bg-gray-500 cursor-not-allowed"
-                        : ""
-                    }`}
                     disabled={shouldDisableForm()}
+                    className={`bg-luni-blue hover:bg-luni-dark-blue text-white font-bold py-2 px-6 rounded-xl focus:outline-none focus:shadow-outline w-36 ${
+                      shouldDisableForm() ? "opacity-50 cursor-not-allowed" : ""
+                    }`}
                   >
                     Login
                   </button>
                   <button
                     type="button"
-                    className="mt-4 hover:text-luni-dark-blue text-luni-blue font-bold py-2 px-6 rounded-xl focus:outline-none focus:shadow-outline w-36"
+                    disabled={shouldDisableForm()}
+                    className={`mt-4 hover:text-luni-dark-blue text-luni-blue font-bold py-2 px-6 rounded-xl focus:outline-none focus:shadow-outline w-36 ${
+                      shouldDisableForm() ? "opacity-50 cursor-not-allowed" : ""
+                    }`}
                     onClick={() => setIsRegistering(true)}
                   >
                     Register
                   </button>
                 </div>
-                {errorMessage && (
+                {errorMessage && !errorMessage.includes("Display name") && (
                   <div className="text-red-600 bg-red-100 border border-red-400 text-sm p-3 rounded mt-4">
                     {errorMessage}
                   </div>
