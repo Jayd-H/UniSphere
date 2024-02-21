0e52fe3700d6769388e9a2c5ad080b82c1b06422
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-21 06:21:24 +0000

    Restyled to look a lot cleaner

--- Client/src/Components/LoginForm.tsx
+++ Client/src/Components/LoginForm.tsx
@@ -40,7 +40,7 @@ const LoginForm = () => {
   };
 
   const getInputBorderClass = (isValid: boolean) =>
-    isValid ? "border-gray-300" : "border-red-500";
+    isValid ? "border-gray-400" : "border-red-500";
 
   const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -52,30 +52,32 @@ const LoginForm = () => {
   return (
     <div className="flex justify-center items-center h-screen glowing-background font-arimo">
       <motion.div
-        className="frosted-glass w-full max-w-xs p-4 rounded-lg shadow-lg relative"
-        initial={{ opacity: 0, y: 300 }}
+        className="frosted-glass w-full max-w-xs p-4 shadow-lg relative rounded-lg"
+        initial={{ opacity: 0, y: 25 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ type: "spring", stiffness: 120, damping: 25 }}
+        transition={{ type: "spring", stiffness: 120, damping: 35}}
       >
         <div className="flex flex-col items-center p-8">
-          <div className="logo-container transition-transform text-luni-black pb-2">
+          <div className="logo-container transition-transform text-luni-black mb-1 -mt-4">
             <motion.div
               whileHover={{ rotate: 180, scale: 1.1 }}
               transition={{ duration: 0.5 }}
             >
-              <GlobeAltIcon className="h-15 w-15" />
+              <GlobeAltIcon className="w-8 h-8" />
             </motion.div>
-            <span className="text-sm font-semibold mt-2">Login</span>
           </div>
-          <form onSubmit={handleLogin} className="space-y-8">
+          <span className="text-sm font-semibold font-arimo">UniSphere</span>
+          <h1 className="text-xl font-semibold mt-6 font-montserrat text-luni-black">Welcome back</h1>
+          <h1 className="text-m font-semibold -mt-1 font-montserrat text-luni-black">Login to your account</h1>
+          <form onSubmit={handleLogin} className="space-y-8 mt-10">
             <div className="relative">
-              <UserIcon className="h-5 w-5 text-luni-grey absolute left-2 inset-y-0 flex items-center mt-2.5" />
+              <UserIcon className="h-5 w-5 text-luni-black absolute left-1 inset-y-0 flex items-center mt-2.5" />
               <input
                 type="text"
                 id="username"
                 value={formData.username}
                 onChange={(e) => handleInputChange(e, "username")}
-                className={`py-2 pl-10 block w-full bg-transparent border-0 border-b-2 focus:border-luni-blue outline-none font-montserrat placeholder-luni-grey ${getInputBorderClass(
+                className={`py-2 pl-8 block w-full bg-transparent border-0 border-b-2 focus:border-luni-blue outline-none font-montserrat placeholder-luni-grey ${getInputBorderClass(
                   usernameChecks.lengthCheck && usernameChecks.spaceCheck
                 )}`}
                 placeholder="Username..."
@@ -83,13 +85,13 @@ const LoginForm = () => {
               />
             </div>
             <div className="relative">
-              <LockClosedIcon className="h-5 w-5 text-luni-grey absolute left-2 inset-y-0 flex items-center mt-2.5" />
+              <LockClosedIcon className="h-5 w-5 text-luni-black absolute left-1 inset-y-0 flex items-center mt-2.5" />
               <input
                 type="password"
                 id="password"
                 value={formData.password}
                 onChange={(e) => handleInputChange(e, "password")}
-                className={`py-2 pl-10 block w-full bg-transparent border-0 border-b-2 focus:border-luni-blue outline-none font-montserrat placeholder-luni-grey ${getInputBorderClass(
+                className={`py-2 pl-8 block w-full bg-transparent border-0 border-b-2 focus:border-luni-blue outline-none font-montserrat placeholder-luni-grey ${getInputBorderClass(
                   passwordChecks.lengthCheck
                 )}`}
                 placeholder="Password..."
@@ -100,16 +102,16 @@ const LoginForm = () => {
               <button
                 type="submit"
                 disabled={shouldDisableForm()}
-                className={`bg-luni-blue hover:bg-luni-dark-blue text-white font-bold py-2 px-6 rounded-xl focus:outline-none focus:shadow-outline w-36 ${
+                className={`bg-luni-blue hover:bg-luni-dark-blue text-white font-bold py-2 px-6 rounded-l focus:outline-none focus:shadow-outline w-40 font-montserrat ${
                   shouldDisableForm() ? "opacity-50 cursor-not-allowed" : ""
                 }`}
               >
                 Login
               </button>
-              <div className="flex items-center justify-center mt-8 -mb-8 text-luni-grey">
+              <div className="flex items-center justify-center mt-8 -mb-8">
                 <Link
                   to="/register"
-                  className="hover:text-luni-dark-blue text-luni-blue font-bold"
+                  className="hover:text-luni-dark-blue text-luni-blue font-bold text-sm font-montserrat"
                 >
                   Sign up
                 </Link>
