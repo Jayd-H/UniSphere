7feee88ec2bc297ee06a39fd0d27eb91bb6ce282
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-29 03:22:22 +0000

    feat: style replybox a bit nicer

--- Client/src/Components/ReplyBox.tsx
+++ Client/src/Components/ReplyBox.tsx
@@ -4,9 +4,10 @@ import { motion } from "framer-motion";
 // Define the type for the props
 interface ReplyBoxProps {
   onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+  displayName: string;
 }
 
-const ReplyBox: React.FC<ReplyBoxProps> = ({ onSubmit }) => {
+const ReplyBox: React.FC<ReplyBoxProps> = ({ onSubmit, displayName }) => {
   return (
     <motion.div
       className="pl-4 py-2 bg-luni-lighter-grey my-2"
@@ -17,16 +18,25 @@ const ReplyBox: React.FC<ReplyBoxProps> = ({ onSubmit }) => {
         transition: { ease: "easeOut", duration: 0.4 },
       }}
     >
-      <form onSubmit={onSubmit}>
+      {/* Adding a header similar to a reply */}
+      <hr className="border-luni-lighter-grey w-3/4 mx-auto -mt-2 pt-2" />
+      <div className="mb-1">
+        <span className="font-semibold text-md">{displayName}</span>
+        {/* Optionally, you can add a static or dynamically generated timestamp here */}
+        <span className="text-xs text-luni-grey ml-2">
+          {/* Dynamic or static timestamp if needed */}
+        </span>
+      </div>
+      <form onSubmit={onSubmit} className="flex justify-between">
         <input
           type="text"
           placeholder="Write a reply..."
-          className="w-full p-2 text-sm text-luni-black bg-white rounded-md border border-luni-light-grey focus:outline-none focus:border-luni-grey"
+          className="text-sm text-luni-black flex-grow mr-4 p-2 bg-white rounded-md border border-luni-light-grey focus:outline-none focus:border-luni-grey"
           name="replyContent"
         />
         <button
           type="submit"
-          className="mt-2 px-4 py-2 bg-luni-blue text-white rounded-md hover:bg-luni-dark-blue focus:outline-none"
+          className="flex-shrink-0 ml-4 self-end bg-luni-blue text-white rounded-md px-4 py-2 hover:bg-luni-dark-blue focus:outline-none"
         >
           Reply
         </button>
