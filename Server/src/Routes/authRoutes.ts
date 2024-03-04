ad32e4f94f11a12c5fc337e321927ef75786566d
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-04 08:48:18 +0000

    feat: add routing for register logic

--- Server/src/Routes/authRoutes.ts
+++ Server/src/Routes/authRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { login } from '../Controllers/authController';
+import { register } from '../Controllers/authController';
 
 const router = Router();
 
 router.post('/login', login);
+router.post('/register', register);
 
 export default router;
