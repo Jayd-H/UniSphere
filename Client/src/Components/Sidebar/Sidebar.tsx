292b655109bb092cb55876c35ca7381c64a592bb
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-06 08:55:03 +0000

    feat: adjust settings link margin

--- Client/src/Components/Sidebar/Sidebar.tsx
+++ Client/src/Components/Sidebar/Sidebar.tsx
@@ -72,7 +72,7 @@ const Sidebar = () => {
           isMobile={isMobile}
         />
       </div>
-      <div className="mb-2">
+      <div className="mb-3">
         <SidebarLink
           to="/settings"
           icon={<CogIcon />}
