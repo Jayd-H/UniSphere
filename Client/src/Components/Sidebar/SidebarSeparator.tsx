f77fb51c9928f13101746beac108ba9ed5e38b60
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-06 08:15:04 +0000

    feat: adjusted y margin

--- Client/src/Components/Sidebar/SidebarSeparator.tsx
+++ Client/src/Components/Sidebar/SidebarSeparator.tsx
@@ -1,5 +1,5 @@
 const SidebarSeparator = () => {
-  return <div className="border-b border-gray-300 mx-auto w-1/2 my-4"></div>;
+  return <div className="border-b border-gray-300 mx-auto w-1/2 my-2"></div>;
 };
 
 export default SidebarSeparator;
