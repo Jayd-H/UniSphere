9713306f1f03fe2b5c65472cd673ee6db637e4d4
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-06 08:10:25 +0000

    chore: remove whitespace

--- Client/src/Components/Sidebar/SidebarSeparator.tsx
+++ Client/src/Components/Sidebar/SidebarSeparator.tsx
@@ -1,6 +1,5 @@
 const SidebarSeparator = () => {
-    return <div className="border-b border-gray-300 mx-auto w-1/2 my-4"></div>;
-  };
-  
-  export default SidebarSeparator;
-  
+  return <div className="border-b border-gray-300 mx-auto w-1/2 my-4"></div>;
+};
+
+export default SidebarSeparator;
