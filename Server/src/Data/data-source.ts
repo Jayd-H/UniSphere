1dea2938ed05010684157de73246b080dfcc4f1a
SamClubley <114992298+SamClubley@users.noreply.github.com>
2024-03-08 23:41:39 +0000

    Updated data-source.ts to inlude the societies entity

--- Server/src/Data/data-source.ts
+++ Server/src/Data/data-source.ts
@@ -7,6 +7,7 @@ import { Posts } from './Posts'
 import { Replies } from './Replies'
 import { RepliesUser } from './RepliesUser'
 import { UserPosts } from "./UserPosts"
+import { Societies } from "./Societies"
 
 dotenv.config()
 
@@ -22,7 +23,8 @@ export const Database = new DataSource({
         Posts,
         Replies,
         RepliesUser,
-        UserPosts
+        UserPosts,
+        Societies
     ],
     ssl: false
 })
