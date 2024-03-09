b6739deddf4bb1a0e27b896242465801d762c1fd
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-09 17:22:04 +0000

    fix: avoided mixing static and instance methods

--- Server/src/Data/User.ts
+++ Server/src/Data/User.ts
@@ -1,23 +1,24 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { DataSource, Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { BaseEntity } from 'typeorm'
 
-@Entity({database: "unisphere",name:"user"})
-export class User extends BaseEntity {
+@Entity({database: "unisphere", name: "user"})
+export class User extends BaseEntity{
     @PrimaryGeneratedColumn("increment")
-    id: number
+    id: number;
 
     @Column("varchar", { length: 32 })
-    username: string
+    username: string;
 
     @Column("varchar", { length: 72 })
-    hash: string
+    hash: string;
 
     @Column("varchar", { length: 64 })
-    displayName: string
+    displayName: string;
 
-    static passwordByUsername(username: string) {
-        return this.find({
+    static async passwordByUsername(dataSource: DataSource, username: string) {
+        return dataSource.getRepository(User).findOne({
             select: { hash: true },
             where: { username: username }
-        })
+        });
     }
 }
