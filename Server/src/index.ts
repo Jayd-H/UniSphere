effb531ad5b8f6914606930419faef758bdd9485
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-09 11:43:33 +0000

    fix: update for vercel

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -7,21 +7,28 @@ import { Database } from './Data/data-source';
 dotenv.config();
 
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 Database.initialize()
     .then(() => {
-        console.log("The database has been initialized!")
+        console.log("The database has been initialized!");
     })
     .catch((err: any) => {
-        console.error("Error during database initialization.", err)
-    })
+        console.error("Error during database initialization.", err);
+    });
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Express server is up and running!");
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+export default app;
