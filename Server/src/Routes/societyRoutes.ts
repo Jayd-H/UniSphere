14b63af974a366a32de5c1eed64c64647ba4c487
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-21 04:39:11 +0000

    chore: create route for getusersociety

--- Server/src/Routes/societyRoutes.ts
+++ Server/src/Routes/societyRoutes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import { authenticateToken } from '../Middleware/authMiddleware';
+import { getUserSocieties } from '../Controllers/societyController';
 import { getAllSocieties } from '../Controllers/societyController';
 
 const router = Router();
 
 router.get('/all', getAllSocieties);
+router.get('/user', authenticateToken, getUserSocieties);
 
 export default router;
