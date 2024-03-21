44ba24d196ab2283fd66cfb9202efad6fbd8d6ab
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-21 15:24:29 +0000

    feat: create basic way to show societies

--- Client/src/Pages/SocietiesPage.tsx
+++ Client/src/Pages/SocietiesPage.tsx
@@ -1,9 +1,49 @@
-const HomePage = () => {
+import React, { useEffect, useState } from "react";
+import Society from "../Components/Society";
+
+interface SocietyData {
+  id: number;
+  name: string;
+  description: string;
+}
+
+const SocietiesPage: React.FC = () => {
+  const [societies, setSocieties] = useState<SocietyData[]>([]);
+
+  useEffect(() => {
+    const fetchSocieties = async () => {
+      try {
+        const response = await fetch(
+          "https://unispherebackend.vercel.app/api/societies/all"
+        );
+        const data = await response.json();
+        if (data.success) {
+          setSocieties(data.data);
+        } else {
+          console.error("Failed to fetch societies:", data.message);
+        }
+      } catch (error) {
+        console.error("Error fetching societies:", error);
+      }
+    };
+
+    fetchSocieties();
+  }, []);
+
   return (
     <div>
-      <h1>societies</h1>
+      <h1 className="text-3xl font-bold underline">Societies</h1>
+      <div>
+        {societies.map((society) => (
+          <Society
+            key={society.id}
+            name={society.name}
+            description={society.description}
+          />
+        ))}
+      </div>
     </div>
   );
 };
 
-export default HomePage;
+export default SocietiesPage;
