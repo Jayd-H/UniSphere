9acc048e1fc2b6e5b2cf070eb4209995cb836c11
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-21 01:15:30 +0000

    feat: create simple route protector

---
+++ Client/src/Components/ProtectedRoute.tsx
@@ -0,0 +1,9 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+const ProtectedRoute = () => {
+  const isAuthenticated = localStorage.getItem("token");
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+};
+
+export default ProtectedRoute;
