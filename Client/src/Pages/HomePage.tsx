6987135400edc506c6609908e4168a58ef73a1e0
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-30 14:19:46 +0000

    Revert "feat: create call function for api"
    This reverts commit 8ea44a70a064126abdb1e0098b831705f149fb45.

--- Client/src/Pages/HomePage.tsx
+++ Client/src/Pages/HomePage.tsx
@@ -1,34 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Feed from "../Components/Home/Feed/Feed";
 import PostBox from "../Components/Home/PostBox/PostBox";
-import { getUserProfile } from "../api/userApi";
-import { User } from "../types/User";
-import { Society } from "../types/Society";
 
 const HomePage: React.FC = () => {
-  const [user, setUser] = useState<User | null>(null);
-  const [societies, setSocieties] = useState<Society[]>([]);
-
-  useEffect(() => {
-    const fetchUserProfile = async () => {
-      try {
-        const token = localStorage.getItem("token");
-        if (token) {
-          const userProfile = await getUserProfile(token);
-          setUser(userProfile);
-          setSocieties(userProfile.societies);
-        }
-      } catch (error) {
-        console.error("Failed to fetch user profile:", error);
-      }
-    };
-
-    fetchUserProfile();
-  }, []);
-
   return (
     <>
-      {user && <PostBox user={user} societies={societies} />}
+      <PostBox />
       <div>
         <br />
       </div>
