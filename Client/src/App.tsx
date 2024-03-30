958b1ae3417e74133616e1ea439baaf652ce7344
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-30 08:18:31 +0000

    chore: replace sidebar with navbar

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -4,22 +4,29 @@ import {
   Route,
   Outlet,
 } from "react-router-dom";
+import { motion, useScroll, useTransform } from "framer-motion";
 import ProtectedRoute from "./Components/ProtectedRoute";
-import LoginForm from "./Components/Forms/LoginForm";
+import LoginForm from "./Pages/LoginPage";
 import HomePage from "./Pages/HomePage";
 import EventsPage from "./Pages/EventsPage";
 import SocietiesPage from "./Pages/SocietiesPage";
 import UserSettingsPage from "./Pages/UserSettingsPage";
-import Sidebar from "./Components/Sidebar/Sidebar";
-import RegisterForm from "./Components/Forms/RegisterForm";
+import Navbar from "./Components/Navbar";
+import RegisterForm from "./Pages/RegisterPage";
 import SplashPage from "./Pages/SplashPage";
 import NotFoundPage from "./Pages/NotFoundPage";
 
+// TODO CURRENTLY NOTHING HAPPENS ON SCROLL, NAVBAR SHOULD HIDE ON SCROLL DOWN AND SHOW ON SCROLL UP
 const LayoutWithSidebar = () => {
+  const { scrollY } = useScroll();
+  const y = useTransform(scrollY, [0, 100], [0, -100]);
+
   return (
-    <div className="flex h-screen bg-luni-light-blue">
-      <Sidebar />
-      <div className="flex-grow p-4 overflow-auto">
+    <div className="flex h-screen bg-white">
+      <motion.div style={{ y }}>
+        <Navbar />
+      </motion.div>
+      <div className="flex-grow overflow-auto mt-16">
         <Outlet /> {/* Render child routes */}
       </div>
     </div>
