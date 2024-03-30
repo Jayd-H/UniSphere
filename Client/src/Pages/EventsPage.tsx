1c7bcdd809f305c75570c1e1a5feecbad5fe24d4
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-30 08:24:04 +0000

    chore: import new components and finish events page

--- Client/src/Pages/EventsPage.tsx
+++ Client/src/Pages/EventsPage.tsx
@@ -1,9 +1,17 @@
-const HomePage = () => {
+import React from "react";
+import EventsFeed from "../Components/Events/Feed/EventsFeed";
+import EventsPostBox from "../Components/Events/PostBox/EventsPostBox";
+
+const EventsPage: React.FC = () => {
   return (
-    <div>
-      <h1>events</h1>
-    </div>
+    <>
+      <EventsPostBox />
+      <div>
+        <br />
+      </div>
+      <EventsFeed />
+    </>
   );
 };
 
-export default HomePage;
+export default EventsPage;
