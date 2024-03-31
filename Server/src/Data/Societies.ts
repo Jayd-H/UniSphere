e0983189e9af6c1edb9a51a5162f11c7706520fa
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-31 00:05:28 +0000

    chore: consistent naming convention

--- Server/src/Data/Societies.ts
+++ Server/src/Data/Societies.ts
@@ -1,17 +1,16 @@
 import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
 
 @Entity({database: "unisphere",name:"societies"})
-export class Societies extends BaseEntity 
-{
-    @PrimaryGeneratedColumn("increment")
-    id: number
+export class Societies extends BaseEntity {
+  @PrimaryGeneratedColumn("increment")
+  id: number
 
-    @Column("varchar", { length: 96 })
-    societyName: string
+  @Column("varchar", { length: 96 })
+  societyName: string
 
-    @Column("varchar", {length: 512})
-    description: string
+  @Column("varchar", {length: 512})
+  description: string
 
-    @Column("varchar", {length: 255})
-    imageURL: string
+  @Column("varchar", {length: 255})
+  imageURL: string
 }
