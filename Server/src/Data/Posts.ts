34a563c1fcb9cc85beb5d7aada1b133224033ced
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-31 00:05:11 +0000

    fix: remove likesnum

--- Server/src/Data/Posts.ts
+++ Server/src/Data/Posts.ts
@@ -1,26 +1,23 @@
 import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm'
-import { User } from './User'
+import { Users } from './Users'
 import { Societies } from './Societies'
 
 @Entity({database: "unisphere",name:"posts"})
 export class Posts extends BaseEntity {
-    @PrimaryGeneratedColumn("increment")
-    id: number
+  @PrimaryGeneratedColumn("increment")
+  id: number
 
-    @Column("varchar", { length: 512 })
-    content: string
+  @Column("varchar", { length: 512 })
+  content: string
 
-    @Column("varchar", { length: 32 })
-    timestamp: string
+  @Column("varchar", { length: 32 })
+  timestamp: string
 
-    @Column("int")
-    likesNum: number
-    
-    @ManyToOne(() => Societies, {cascade: true})
-    @JoinColumn({name: "id"})
-    societyId: number
+  @ManyToOne(() => Societies, {cascade: true})
+  @JoinColumn({name: "id"})
+  societyId: number
 
-    @ManyToOne(() => User, {cascade: true})
-    @JoinColumn({name: "id"})
-    userId: number
+  @ManyToOne(() => Users, {cascade: true})
+  @JoinColumn({name: "id"})
+  userId: number
 }
