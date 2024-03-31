d5acbec2f66f67e6050304c2dd645174da26c08e
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-31 00:07:50 +0000

    chore: consistent naming convention

--- Server/src/Controllers/authController.ts
+++ Server/src/Controllers/authController.ts
@@ -1,38 +1,35 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
-import { RowDataPacket } from 'mysql2';
 import { Database } from '../Data/data-source';
 import jwt from 'jsonwebtoken';
-import { User } from '../Data/User';
-
-interface UserPasswordRow extends RowDataPacket {
-  hash: string;
-}
+import { Users } from '../Data/Users';
 
 export const login = async (req: Request, res: Response) => {
-  const { username, password } = req.body;
-  if (!username || !password) {
+  const { userName, password } = req.body;
+
+  if (!userName || !password) {
     return res.status(400).json({ success: false, message: "Username and password are required" });
   }
-  
+
   try {
-    const existingUser = await Database.getRepository(User).findOne({
-      where: { username }
+    const existingUser = await Database.getRepository(Users).findOne({
+      where: { userName }
     });
 
     if (!existingUser) {
-      console.error(`Login error: User with username '${username}' not found.`);
+      console.error(`Login error: User with username '${userName}' not found.`);
       return res.status(404).json({ success: false, message: "User not found" });
     }
-    
+
     const isMatch = await bcrypt.compare(password, existingUser.hash);
 
     if (isMatch) {
       // User authenticated, create JWT
-      const userPayload = { id: existingUser.id, username: existingUser.username };
+      const userPayload = { id: existingUser.id, userName: existingUser.userName };
 
       // Assert ACCESS_TOKEN_SECRET is not undefined
       const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET!;
+
       if (!accessTokenSecret) {
         console.error('ACCESS_TOKEN_SECRET is not defined.');
         return res.status(500).json({ success: false, message: "Server configuration error" });
@@ -40,15 +37,15 @@ export const login = async (req: Request, res: Response) => {
 
       const accessToken = jwt.sign(userPayload, accessTokenSecret, { expiresIn: '1h' });
 
-      console.log(`User '${username}' logged in successfully.`);
+      console.log(`User '${userName}' logged in successfully.`);
       res.status(200).json({ success: true, message: "Login successful", accessToken });
     } else {
-      console.error(`Login error: Invalid credentials for user '${username}'.`);
+      console.error(`Login error: Invalid credentials for user '${userName}'.`);
       res.status(401).json({ success: false, message: "Invalid credentials" });
     }
   } catch (error: any) {
     console.error(`Login error: ${error.message}`, {
-      username,
+      userName,
       error: error.stack,
     });
     res.status(500).json({ success: false, message: "Server error" });
@@ -56,25 +53,27 @@ export const login = async (req: Request, res: Response) => {
 };
 
 export const register = async (req: Request, res: Response) => {
-  const { username, password, displayName } = req.body;
+  const { userName, password, displayName } = req.body;
 
   try {
-    const existingUser = await Database.getRepository(User).findOne({
-      where: { username }
+    const existingUser = await Database.getRepository(Users).findOne({
+      where: { userName }
     });
-    
+
     if (existingUser) {
       return res.status(409).json({ success: false, message: "Username is already taken." });
     }
 
     // If username doesn't exist, proceed with creating the new user
     const hash = await bcrypt.hash(password, 10);
-    const user = new User();
-    user.username = username;
+
+    const user = new Users();
+    user.userName = userName;
     user.hash = hash;
     user.displayName = displayName;
+
     await user.save();
-    
+
     return res.status(200).json({ success: true, message: "User registered successfully." });
   } catch (error: any) {
     console.error("Error registering user:", error);
