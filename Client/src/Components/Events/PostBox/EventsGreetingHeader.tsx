11c9224765402e2dd58cdd269590a18f71c29ac4
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-31 16:30:19 +0100

    feat: add functionality to pass in logged in users displayname

--- Client/src/Components/Events/PostBox/EventsGreetingHeader.tsx
+++ Client/src/Components/Events/PostBox/EventsGreetingHeader.tsx
@@ -1,7 +1,13 @@
 import { FC } from "react";
 import { motion } from "framer-motion";
 
-const EventsGreetingHeader: FC<{}> = () => (
+interface EventsGreetingHeaderProps {
+  displayName: string;
+}
+
+const EventsGreetingHeader: FC<EventsGreetingHeaderProps> = ({
+  displayName,
+}) => (
   <motion.div
     className="flex justify-center text-center font-montserrat"
     initial={{ opacity: 0, y: 10 }}
@@ -9,7 +15,9 @@ const EventsGreetingHeader: FC<{}> = () => (
     transition={{ duration: 0.8, ease: "easeOut" }}
   >
     <div className="grid grid-cols-1">
-      <h1 className="text-2xl font-semibold font-montserrat-alt">Bored?</h1>
+      <h1 className="text-2xl font-semibold font-montserrat-alt">
+        Hey there {displayName}!
+      </h1>
       <h1 className="text-xl -mt-3">Join an event!</h1>
       <div className="border-b-2 border-dashed border-muted-mint"></div>
     </div>
