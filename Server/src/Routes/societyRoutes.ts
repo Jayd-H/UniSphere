9347da643d25154eac26ad359ec4681d6f10b6f1
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-31 16:20:55 +0100

    feat: create routes for society functions

--- Server/src/Routes/societyRoutes.ts
+++ Server/src/Routes/societyRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { authenticateToken } from '../Middleware/authMiddleware';
-import { getUserSocieties } from '../Controllers/societyController';
-import { getAllSocieties } from '../Controllers/societyController';
-import { getSpecificSociety } from '../Controllers/societyController';
+import { getUserSocieties, getAllSocieties, getSpecificSociety } from '../Controllers/societyController';
+import { fetchEventPosts, likeEventPost, unlikeEventPost, } from '../Controllers/eventPostsController';
+
 const router = Router();
 
 router.get('/all', getAllSocieties);
 router.get('/user', authenticateToken, getUserSocieties);
 router.get('/One', getSpecificSociety);
+
 export default router;
