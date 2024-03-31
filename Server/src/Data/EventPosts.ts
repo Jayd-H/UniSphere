8c12310ac59d3019100b67ffbf02f5de4f931859
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-31 00:04:39 +0000

    chore: consistent naming convention

--- Server/src/Data/EventPosts.ts
+++ Server/src/Data/EventPosts.ts
@@ -1,35 +1,32 @@
 import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm'
-import { User } from './User'
+import { Users } from './Users'
 import { Societies } from './Societies'
 
-@Entity({database: "unisphere",name:"eventposts"})
-export class Posts extends BaseEntity {
-    @PrimaryGeneratedColumn("increment")
-    id: number
+@Entity({database: "unisphere",name:"event_posts"})
+export class EventPosts extends BaseEntity {
+  @PrimaryGeneratedColumn("increment")
+  id: number
 
-    @Column("varchar", { length: 512 })
-    content: string
+  @Column("varchar", { length: 512 })
+  eventPost: string
 
-    @Column("varchar", { length: 64 })
-    location: string
+  @Column("varchar", { length: 64 })
+  location: string
 
-    @Column("varchar", { length: 32 })
-    timestamp: string
+  @Column("varchar", { length: 32 })
+  timestamp: string
 
-    @Column("varchar", { length: 32 })
-    eventTime: string
+  @Column("varchar", { length: 32 })
+  eventTime: string
 
-    @Column("varchar", { length: 32 })
-    eventtype: string
+  @Column("varchar", { length: 32 })
+  eventType: string
 
-    @Column("int")
-    likesNum: number
+  @ManyToOne(() => Societies, {cascade: true})
+  @JoinColumn({name: "id"})
+  societyId: number
 
-    @ManyToOne(() => Societies, {cascade: true})
-    @JoinColumn({name: "id"})
-    societyId: number
-
-    @ManyToOne(() => User, {cascade: true})
-    @JoinColumn({name: "id"})
-    userId: number
+  @ManyToOne(() => Users, {cascade: true})
+  @JoinColumn({name: "id"})
+  userId: number
 }
