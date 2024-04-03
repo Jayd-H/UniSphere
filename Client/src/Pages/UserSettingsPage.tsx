edc710b9ba8c46f3180d33fd72c9ae3b5f7c3dcb
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-04-03 19:17:36 +0100

    feat: abstract into more components

--- Client/src/Pages/UserSettingsPage.tsx
+++ Client/src/Pages/UserSettingsPage.tsx
@@ -1,84 +1,42 @@
 import React, { useState, useEffect } from "react";
-import { AnimatePresence, motion, spring } from "framer-motion";
-import { PencilIcon } from "@heroicons/react/24/solid";
-import SocietyCard from "../Components/Societies/SocietyCard";
+import { motion, spring } from "framer-motion";
 import AlertMessage from "../Components/Common/AlertMessage";
-import { mockUser } from "../Components/UserSettings/MockData";
-import Popup from "../Components/Common/Popup";
-import ExpandedSocietyCard from "../Components/Societies/ExpandedSocietyCard";
-
-import { getPopupProps } from "../Components/Common/PopupProps";
-
-interface SocietyData {
-  id: number;
-  societyName: string;
-  description: string;
-  imageURL: string;
-}
+import { useUserContext } from "../UserContext";
+import { fetchUserDetails } from "../api/settingsAPI";
+import UserDetails from "../Components/UserSettings/UserDetails";
+import SocietiesList from "../Components/UserSettings/SocietiesList";
+import AccountActions from "../Components/UserSettings/AccountActions";
 
 const UserSettingsPage: React.FC = () => {
-  const [user, _setUser] = useState(mockUser);
-  const [societies, setSocieties] = useState<SocietyData[]>([]);
-  const [popupType, setPopupType] = useState<string | null>(null);
+  const { user, setUser, societies, setSocieties } = useUserContext();
+  const [userDetails, setUserDetails] = useState({
+    username: "",
+    displayName: "",
+    societyCount: 0,
+    replyCount: 0,
+    regularPostCount: 0,
+    eventReplyCount: 0,
+    eventPostCount: 0,
+  });
   const [isAlertVisible, setIsAlertVisible] = useState(false);
   const [alertMessage, setAlertMessage] = useState("");
   const [isAlertSuccess, setIsAlertSuccess] = useState(false);
 
-  const [expandedSociety, setExpandedSociety] = useState<SocietyData | null>(
-    null
-  );
-
-  const handleCardClick = (society: SocietyData) => {
-    setExpandedSociety(society);
-  };
-
-  const handleCloseExpandedCard = () => {
-    setExpandedSociety(null);
-  };
-
-  // TODO: THIS IS A TERRIBLE WAY TO DO THIS, REPLACE WITH A FUNCTION THAT FETCHES SOCIETIES BY USER ID EVENTUALLY
   useEffect(() => {
-    const fetchSocieties = async () => {
+    const fetchData = async () => {
       try {
-        const response = await fetch(
-          "https://unispherebackend.vercel.app/api/societies/all"
-        );
-        const data = await response.json();
-        if (data.success) {
-          const userSocieties = data.data.filter((society: SocietyData) =>
-            user.societies.includes(society.societyName)
-          );
-          setSocieties(userSocieties);
-        } else {
-          console.error("Failed to fetch societies:", data.message);
+        const token = localStorage.getItem("token");
+        if (token && user) {
+          const data = await fetchUserDetails(token);
+          setUserDetails(data);
         }
       } catch (error) {
-        console.error("Error fetching societies:", error);
+        console.error("Error fetching user details:", error);
       }
     };
-    fetchSocieties();
-  }, [user.societies]);
 
-  const handleConfirm = (_data: any) => {
-    // TODO: Implement confirm logic based on popupType
-    setPopupType(null);
-    showAlert("Changes saved successfully", true);
-  };
-
-  const handleCancel = () => {
-    setPopupType(null);
-  };
-
-  const handleLeaveSociety = (societyId: number) => {
-    // TODO: Implement leave society logic
-    setSocieties(societies.filter((society) => society.id !== societyId));
-    showAlert(
-      `Left ${
-        societies.find((society) => society.id === societyId)?.societyName
-      }`,
-      true
-    );
-  };
+    fetchData();
+  }, [user]);
 
   const showAlert = (message: string, isSuccess: boolean) => {
     setAlertMessage(message);
@@ -103,19 +61,20 @@ const UserSettingsPage: React.FC = () => {
   };
 
   const childVariants = {
-    hidden: { opacity: 0, y: 10 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.3 } },
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        delayChildren: 0.2,
+        staggerChildren: 0.1,
+        type: spring,
+      },
+    },
   };
-
-  const iconVariants = {
-    hover: { scale: 1.2, transition: { duration: 0.2, type: spring } },
-    tap: { scale: 0.7, transition: { duration: 0.2, type: spring } },
-  };
-
-  const buttonVariants = {
-    hover: { scale: 1.1, transition: { duration: 0.2, type: spring } },
-    tap: { scale: 0.9, transition: { duration: 0.2 } },
-  };
+  if (!user) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <motion.div
@@ -130,128 +89,19 @@ const UserSettingsPage: React.FC = () => {
       >
         User Settings
       </motion.h1>
-      <motion.div className="mb-8" variants={childVariants}>
-        <motion.h2
-          className="text-2xl font-bold mb-3 font-montserrat-alt"
-          variants={childVariants}
-        >
-          User Details
-        </motion.h2>
-        <motion.div className="flex items-center mb-2" variants={childVariants}>
-          <span className="font-bold mr-2">Username:</span>
-          <span>{user.username}</span>
-          <motion.div
-            className="h-5 w-5 ml-2 cursor-pointer"
-            variants={iconVariants}
-            whileHover="hover"
-            whileTap="tap"
-            onClick={() => setPopupType("username")}
-          >
-            <PencilIcon />
-          </motion.div>
-        </motion.div>
-        <motion.div className="flex items-center mb-2" variants={childVariants}>
-          <span className="font-bold mr-2">Display Name:</span>
-          <span>{user.displayName}</span>
-          <motion.div
-            className="h-5 w-5 ml-2 cursor-pointer"
-            variants={iconVariants}
-            whileHover="hover"
-            whileTap="tap"
-            onClick={() => setPopupType("displayName")}
-          >
-            <PencilIcon />
-          </motion.div>
-        </motion.div>
-        <motion.div className="mb-2" variants={childVariants}>
-          <span className="font-bold mr-2">Societies:</span>
-          <span>{user.societyCount}</span>
-        </motion.div>
-        <motion.div className="mb-2" variants={childVariants}>
-          <span className="font-bold mr-2">Replies:</span>
-          <span>{user.replyCount}</span>
-        </motion.div>
-        <motion.div className="mb-2" variants={childVariants}>
-          <span className="font-bold mr-2">Posts:</span>
-          <span>{user.regularPostCount}</span>
-        </motion.div>
-        <motion.div className="mb-2" variants={childVariants}>
-          <span className="font-bold mr-2">Event Posts:</span>
-          <span>{user.eventPostCount}</span>
-        </motion.div>
-        <motion.button
-          className="bg-blue text-white px-4 py-2 rounded-md font-semibold mt-4"
-          variants={buttonVariants}
-          whileHover="hover"
-          whileTap="tap"
-          onClick={() => setPopupType("password")}
-        >
-          Change Password
-        </motion.button>
-      </motion.div>
-      <motion.div variants={childVariants}>
-        <motion.h2 className="text-2xl font-bold mb-4" variants={childVariants}>
-          Societies You Are In
-        </motion.h2>
-        <motion.div
-          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8"
-          variants={childVariants}
-        >
-          {societies.map((society) => (
-            <motion.div
-              key={society.id}
-              className="relative"
-              variants={childVariants}
-            >
-              <SocietyCard
-                society={society}
-                onClick={handleCardClick}
-                bin={true}
-                onLeaveSociety={() => handleLeaveSociety(society.id)}
-              />
-            </motion.div>
-          ))}
-        </motion.div>
-      </motion.div>
-      <motion.div className="mt-8" variants={childVariants}>
-        <motion.button
-          className="bg-red text-white px-4 py-2 rounded-md font-semibold mr-4"
-          variants={buttonVariants}
-          whileHover="hover"
-          whileTap="tap"
-          onClick={() => setPopupType("deleteAccount")}
-        >
-          Delete Account
-        </motion.button>
-        <motion.button
-          className="bg-gray-200 text-black px-4 py-2 rounded-md font-semibold"
-          variants={buttonVariants}
-          whileHover="hover"
-          whileTap="tap"
-        >
-          Request Data
-        </motion.button>
-      </motion.div>
-      <AnimatePresence>
-        {popupType && (
-          <Popup
-            {...getPopupProps(
-              popupType,
-              handleConfirm,
-              handleCancel,
-              setPopupType
-            )}
-            onClose={() => setPopupType(null)}
-          />
-        )}
-        {expandedSociety && (
-          <ExpandedSocietyCard
-            society={expandedSociety}
-            onClose={handleCloseExpandedCard}
-            userId={user.id}
-          />
-        )}
-      </AnimatePresence>
+      <UserDetails
+        user={user}
+        userDetails={userDetails}
+        showAlert={showAlert}
+        setUser={setUser}
+      />
+      <SocietiesList
+        societies={societies}
+        setSocieties={setSocieties}
+        showAlert={showAlert}
+        user={user}
+      />
+      <AccountActions showAlert={showAlert} setUser={setUser} />
       <AlertMessage
         message={alertMessage}
         isVisible={isAlertVisible}
