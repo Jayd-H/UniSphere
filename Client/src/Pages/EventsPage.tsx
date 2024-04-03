7e2b15a5ebc7e3400b09b970b4dbce6b48419187
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-04-03 19:19:22 +0100

    fix: remove eventspostbox, as it is now called in eventsfeed

--- Client/src/Pages/EventsPage.tsx
+++ Client/src/Pages/EventsPage.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 import EventsFeed from "../Components/Events/Feed/EventsFeed";
-import EventsPostBox from "../Components/Events/PostBox/EventsPostBox";
 
 const EventsPage: React.FC = () => {
   return (
     <>
-      <EventsPostBox />
-      <div>
-        <br />
-      </div>
       <EventsFeed />
     </>
   );
