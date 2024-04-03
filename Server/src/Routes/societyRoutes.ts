a97549bbd4925ef50dca6b271862349788a0b3b9
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-04-03 19:08:00 +0100

    feat: add routes for societycontroller

--- Server/src/Routes/societyRoutes.ts
+++ Server/src/Routes/societyRoutes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 import { authenticateToken } from '../Middleware/authMiddleware';
-import { getUserSocieties, getAllSocieties, getSpecificSociety } from '../Controllers/societyController';
-import { fetchEventPosts, likeEventPost, unlikeEventPost, } from '../Controllers/eventPostsController';
+import { getUserSocieties, getAllSocieties, getSpecificSociety, getSocietyPosts, joinSociety, leaveSociety, getSocietyMemberCount } from '../Controllers/societyController';
 
 const router = Router();
 
 router.get('/all', getAllSocieties);
 router.get('/user', authenticateToken, getUserSocieties);
 router.get('/One', getSpecificSociety);
+router.get('/:societyId/posts', authenticateToken, getSocietyPosts);
+
+router.post('/:societyId/join', authenticateToken, joinSociety);
+router.delete('/:societyId/leave', authenticateToken, leaveSociety);
+router.get('/:societyId/members/count', getSocietyMemberCount);
 
 export default router;
