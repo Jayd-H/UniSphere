23740481dc7defec5badda30b3d243f6bc8e54a0
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-04-03 19:32:36 +0100

    feat: change greeting message

--- Client/src/Components/Events/PostBox/EventsGreetingHeader.tsx
+++ Client/src/Components/Events/PostBox/EventsGreetingHeader.tsx
@@ -1,27 +1,21 @@
-import { FC } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 
-interface EventsGreetingHeaderProps {
-  displayName: string;
-}
-
-const EventsGreetingHeader: FC<EventsGreetingHeaderProps> = ({
-  displayName,
-}) => (
-  <motion.div
-    className="flex justify-center text-center font-montserrat"
-    initial={{ opacity: 0, y: 10 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ duration: 0.8, ease: "easeOut" }}
-  >
-    <div className="grid grid-cols-1">
-      <h1 className="text-2xl font-semibold font-montserrat-alt">
-        Hey there {displayName}!
-      </h1>
-      <h1 className="text-xl -mt-3">Join an event!</h1>
-      <div className="border-b-2 border-dashed border-muted-mint"></div>
-    </div>
-  </motion.div>
-);
+const EventsGreetingHeader: React.FC = () => {
+  return (
+    <motion.div
+      className="flex justify-center text-center font-montserrat"
+      initial={{ opacity: 0, y: 10 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8, ease: "easeOut" }}
+    >
+      <div className="grid grid-cols-1">
+        <h1 className="text-2xl font-semibold font-montserrat-alt">Bored?</h1>
+        <h1 className="text-xl -mt-3">Join an event!</h1>
+        <div className="border-b-2 border-dashed border-muted-mint"></div>
+      </div>
+    </motion.div>
+  );
+};
 
 export default EventsGreetingHeader;
