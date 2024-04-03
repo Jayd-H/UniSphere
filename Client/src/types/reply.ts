e43e4d7d6848f6ee5506834029d71d737f65c392
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-04-03 19:15:19 +0100

    fix: add user related fields

--- Client/src/types/reply.ts
+++ Client/src/types/reply.ts
@@ -2,14 +2,10 @@ export interface Reply {
   replyId: number;
   replyContent: string;
   timestamp: string;
-  userId?: number;
-  postId?: number;
-  eventPostId?: number;
-  displayName?: string;
-  user?: {
+  user: {
     id: number;
     displayName: string;
   };
   likesCount: number;
-  isLiked?: boolean;
+  isLiked: boolean;
 }
