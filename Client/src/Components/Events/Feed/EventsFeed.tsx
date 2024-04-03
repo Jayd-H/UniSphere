5653c5f0f4d714a3191d65e10221e24aab080ea2
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-04-03 19:35:25 +0100

    feat: complete eventsfeed functionality, add ability for creation of "fakepost" on eventspost submit
    please see the other feeds description on information of what a fakepost is and what purpose it serves

--- Client/src/Components/Events/Feed/EventsFeed.tsx
+++ Client/src/Components/Events/Feed/EventsFeed.tsx
@@ -1,57 +1,78 @@
-import React, { useEffect, useState } from "react";
-import EventsPost from "./EventsPost";
+import React, { useEffect, useState, useRef } from "react";
 import { motion } from "framer-motion";
 import { fetchEventPosts } from "../../../api/eventsPostsAPI";
-import { useUserContext } from "../../../UserContext";
 import { EventsPost as EventsPostType } from "../../../types/eventsPost";
-
-const postVariants = {
-  initial: { opacity: 0, y: 20 },
-  hidden: { opacity: 0, y: 20 },
-  visible: (index: number) => ({
-    opacity: 1,
-    y: 0,
-    transition: {
-      delay: 0.6 + index * 0.2,
-      duration: 0.6,
-      ease: "easeOut",
-    },
-  }),
-};
+import EventsPost from "./Post/EventsPost";
+import EventsPostBox from "../PostBox/EventsPostBox";
+import Pagination from "../../Home/Feed/Pagination";
 
 const EventsFeed: React.FC = () => {
   const [eventsPosts, setEventsPosts] = useState<EventsPostType[]>([]);
-  const { societies } = useUserContext();
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [error, setError] = useState<Error | null>(null);
+  const feedRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const token = localStorage.getItem("token");
-        if (token && societies) {
-          const societyIds = societies.map((society) => society.id);
-          const eventsPostsData = await fetchEventPosts(societyIds);
-          setEventsPosts(eventsPostsData);
+  const addNewEventsPost = (post: EventsPostType) => {
+    setEventsPosts((prevPosts) => [post, ...prevPosts]);
+  };
+
+  const fetchEventsPostsData = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      if (token) {
+        const response = await fetchEventPosts([], token, page);
+        if (response && response.eventPosts) {
+          const newEventsPosts = response.eventPosts;
+          const totalPages = response.totalPages;
+          setEventsPosts(newEventsPosts);
+          setTotalPages(totalPages);
+        } else {
+          console.error("Invalid response data:", response);
         }
-      } catch (error) {
-        console.error("Error fetching events posts:", error);
       }
-    };
-    fetchData();
-  }, [societies]);
+    } catch (error) {
+      setError(error as Error);
+    }
+  };
+  useEffect(() => {
+    fetchEventsPostsData();
+  }, [page]);
+
+  const handlePageChange = (newPage: number) => {
+    setPage(newPage);
+    if (feedRef.current) {
+      feedRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
-    <div>
-      {eventsPosts.map((eventPost, index) => (
+    <div ref={feedRef}>
+      <EventsPostBox addNewEventsPost={addNewEventsPost} />
+      {eventsPosts.map((eventsPost, index) => (
         <motion.div
-          key={eventPost.id}
-          variants={postVariants}
-          initial="initial"
-          animate="visible"
-          custom={index}
+          key={`${eventsPost.eventsPostId}-${index}`}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.2 * index, duration: 0.6, ease: "easeOut" }}
         >
-          <EventsPost {...eventPost} />
+          <EventsPost {...eventsPost} />
         </motion.div>
       ))}
+      <div className="flex justify-center mt-4">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 5.2, duration: 0.6, ease: "easeOut" }}
+        >
+          <Pagination
+            currentPage={page}
+            totalPages={totalPages}
+            onPageChange={handlePageChange}
+          />
+        </motion.div>
+      </div>
+      {error && <p>Error: {error.message}</p>}
     </div>
   );
 };
