a832d20b5b400f646d7ea3a3cd634b7afe79cbae
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-04-03 19:26:15 +0100

    feat: add functionality to create post

--- Client/src/Components/Home/PostBox/PostBox.tsx
+++ Client/src/Components/Home/PostBox/PostBox.tsx
@@ -7,59 +7,60 @@ import AlertMessage from "../../Common/AlertMessage";
 import { createPost } from "../../../api/postsAPI";
 import { useUserContext } from "../../../UserContext";
 import { Society } from "../../../types/society";
+import { Post as PostType } from "../../../types/post";
 
-const PostBox: React.FC = () => {
+interface PostBoxProps {
+  addNewPost: (post: PostType) => void;
+}
+
+const PostBox: React.FC<PostBoxProps> = ({ addNewPost }) => {
   const [postContent, setPostContent] = useState("");
   const [selectedSociety, setSelectedSociety] = useState<Society | null>(null);
   const [isOpen, setIsOpen] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
   const maxCharacters = 512;
-
   const { user, societies } = useUserContext();
-  let message = "Please select a society before making a post";
+  let message = "Please select a society to create a post";
 
   const handlePostSubmit = async () => {
-    if (selectedSociety) {
+    if (selectedSociety && user) {
       try {
         const token = localStorage.getItem("token");
         if (token) {
-          const sanitizedContent = postContent.replace(/\['"\]/g, "");
-          await createPost(sanitizedContent, selectedSociety.id, token);
+          const sanitizedContent = postContent.replace(/\\['"]]/g, "");
+          const timestamp = new Date().toISOString();
+          const { postId } = await createPost(
+            sanitizedContent,
+            selectedSociety.id,
+            token,
+            timestamp
+          );
+          const newPost: PostType = {
+            postId,
+            postContent: sanitizedContent,
+            timestamp,
+            societyId: selectedSociety.id,
+            societyName: selectedSociety.societyName,
+            user: {
+              id: user.id,
+              displayName: user.displayName,
+            },
+            likesCount: 0,
+            isLiked: false,
+            replyCount: 0,
+            replies: [],
+          };
+          addNewPost(newPost);
           setPostContent("");
         }
       } catch (error) {
         console.error("Error creating post:", error);
+        message = "Error creating post. Please try again.";
         setShowAlert(true);
       }
     } else {
       setShowAlert(true);
     }
-  };
-
-  const containerVariants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: 0.8,
-        staggerChildren: 0.1,
-        ease: "easeInOut",
-      },
-    },
-  };
-
-  const itemVariants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: 0.8,
-        staggerChildren: 0.1,
-        ease: "easeInOut",
-      },
-    },
   };
 
   useEffect(() => {
@@ -76,20 +77,24 @@ const PostBox: React.FC = () => {
   return (
     <motion.div
       className="text-black font-montserrat mt-4"
-      variants={containerVariants}
-      initial="hidden"
-      animate="visible"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8, ease: "easeOut" }}
     >
       <AlertMessage isSuccess={false} message={message} isVisible={showAlert} />
       {user && <GreetingHeader displayName={user.displayName} />}
       <motion.div
         className="bg-white rounded-xl p-6 max-w-2xl mx-auto shadow-sm shadow-muted-mint hover:shadow-mint mt-4"
-        variants={itemVariants}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
       >
         <div className="flex justify-between items-center mb-4">
           <motion.h1
             className="text-lg font-montserrat underline decoration-mint"
-            variants={itemVariants}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, ease: "easeOut" }}
           >
             What's going on?
           </motion.h1>
