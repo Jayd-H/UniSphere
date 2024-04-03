522ca343a06d054d8d102d6d667c586d5b4aa94e
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-04-03 20:16:48 +0100

    chore: dewrap start function
    maybe this might make vercel work?

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -6,7 +6,7 @@ import societyRoutes from './Routes/societyRoutes';
 import { Database } from './Data/data-source';
 import postRoutes from './Routes/postRoutes';
 import userRoutes from './Routes/userRoutes';
-import settingsRoutes  from './Routes/settingsRoutes';
+import settingsRoutes from './Routes/settingsRoutes';
 import eventPostRoutes from './Routes/eventPostRoutes';
 
 dotenv.config();
@@ -14,24 +14,24 @@ dotenv.config();
 const app = express();
 
 async function startServer() {
-  (async () => {
   try {
     await Database.initialize();
     console.log("The database has been initialized!");
 
     app.use(cors({
-        origin: 'https://unispher-e.vercel.app',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization']
-      }));
-    app.use(express.json());
+      origin: 'https://unispher-e.vercel.app',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization']
+    }));
 
+    app.use(express.json());
     app.use('/api/auth', authRoutes);
     app.use('/api/societies', societyRoutes);
     app.use('/api', postRoutes);
     app.use('/api', eventPostRoutes);
     app.use('/api/user', userRoutes);
-    app.use('/api/settings', settingsRoutes)
+    app.use('/api/settings', settingsRoutes);
+
     app.get("/", (req, res) => {
       res.send("Express server is up and running!");
     });
@@ -43,7 +43,6 @@ async function startServer() {
   } catch (err) {
     console.error("Error during database initialization.", err);
   }
-})();
 }
 
 startServer();
