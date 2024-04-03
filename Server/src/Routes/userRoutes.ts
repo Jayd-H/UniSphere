5a969857d8ffb502d38c1512331050e449515219
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-04-03 19:08:28 +0100

    fix: remove useless routes for unused functions

--- Server/src/Routes/userRoutes.ts
+++ Server/src/Routes/userRoutes.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 import { authenticateToken } from '../Middleware/authMiddleware';
-import { getUserData, fetchUserDisplayName, getUserJoinedSocieties } from '../Controllers/userController';
+import { getUserData } from '../Controllers/userController';
 
 const router = Router();
 
 router.get('/', authenticateToken, getUserData);
-router.get('/:userId/societies', authenticateToken, getUserJoinedSocieties);
-router.get('/users/:userId/displayName', fetchUserDisplayName);
 
 export default router;
