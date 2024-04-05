35800d09e2e47cb3b43acf5d77a88dca64b8e12b
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-04-05 22:56:04 +0100

    feat: elements now stack on smaller screens

--- Client/src/Components/Events/Feed/Post/EventsPostHeader.tsx
+++ Client/src/Components/Events/Feed/Post/EventsPostHeader.tsx
@@ -15,25 +15,30 @@ const EventsPostHeader: React.FC<EventsPostHeaderProps> = ({
   societyName,
 }) => {
   return (
-    <div className="flex justify-between items-center mb-3">
-      <motion.div
-        initial={{ opacity: 0, x: -10 }}
-        animate={{ opacity: 1, x: 0 }}
-      >
-        <span className="font-semibold font-montserrat text-lg">
-          {user.displayName}
-        </span>
-        <span className="text-sm text-grey ml-2">{timeSince(timestamp)}</span>
-      </motion.div>
-      <motion.button
-        className="text-md mr-2 px-2.5 py-0.5 rounded font-montserrat border-2 border-mint border-dashed text-black"
-        initial={{ opacity: 0, scale: 0.7 }}
-        animate={{ opacity: 1, scale: 1 }}
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
-      >
-        {societyName}
-      </motion.button>
+    <div className="md:flex md:justify-between items-center mb-3">
+      <div className="flex flex-col items-center md:flex-row md:items-center">
+        <motion.div
+          initial={{ opacity: 0, x: -10 }}
+          animate={{ opacity: 1, x: 0 }}
+          className="text-center md:text-left"
+        >
+          <span className="font-semibold font-montserrat text-lg">
+            {user.displayName}
+          </span>
+          <span className="text-sm text-grey ml-2">{timeSince(timestamp)}</span>
+        </motion.div>
+      </div>
+      <div className="flex justify-center mt-2 md:mt-0">
+        <motion.button
+          className="text-md px-2.5 py-0.5 rounded font-montserrat border-2 border-mint border-dashed text-black"
+          initial={{ opacity: 0, scale: 0.7 }}
+          animate={{ opacity: 1, scale: 1 }}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          {societyName}
+        </motion.button>
+      </div>
     </div>
   );
 };
