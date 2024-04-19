23af1004d459e613e6c7201fe0288467ce8c3a86
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-04-19 21:30:05 +0100

    feat: create joined society widgets

---
+++ Client/src/Components/Home/FeedSocietiesList.tsx
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import SocietyCard from "../Societies/SocietyCard";
+import ExpandedSocietyCard from "../Societies/ExpandedSocietyCard";
+import { Society } from "../../types/society";
+import { leaveSociety } from "../../api/societiesAPI";
+import { User } from "../../types/user";
+
+interface HomeSocietiesListProps {
+  societies: Society[];
+  setSocieties: (societies: Society[]) => void;
+  user: User;
+}
+
+const HomeSocietiesList: React.FC<HomeSocietiesListProps> = ({
+  societies,
+  setSocieties,
+  user,
+}) => {
+  const [expandedSociety, setExpandedSociety] = useState<Society | null>(null);
+
+  const handleCardClick = (society: Society) => {
+    setExpandedSociety(society);
+  };
+
+  const handleCloseExpandedCard = () => {
+    setExpandedSociety(null);
+  };
+
+  const handleLeaveSociety = async (societyId: number) => {
+    try {
+      const token = localStorage.getItem("token");
+      if (token && user) {
+        await leaveSociety(societyId, user.id, token);
+        setSocieties(societies.filter((society) => society.id !== societyId));
+      }
+    } catch (error) {
+      console.error("Error leaving society:", error);
+    }
+  };
+
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.3,
+        delayChildren: 0.6,
+      },
+    },
+  };
+
+  const headingVariants = {
+    hidden: { x: -50, opacity: 0 },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        ease: "easeOut",
+      },
+    },
+  };
+
+  const cardVariants = {
+    hidden: { x: -50, opacity: 0 },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.6,
+        ease: "easeOut",
+      },
+    },
+  };
+
+  return (
+    <div className="hidden xl:block ml-16 mt-9 mr-8 w-64">
+      <motion.div
+        className="mb-8"
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+      >
+        <motion.h2
+          className="text-lg font-semibold mb-4 text-center font-montserrat-alt underline underline-offset-8 decoration-muted-mint decoration-dashed"
+          variants={headingVariants}
+        >
+          Joined Societies
+        </motion.h2>
+        {societies.map((society) => (
+          <motion.div key={society.id} className="mb-4" variants={cardVariants}>
+            <SocietyCard
+              society={society}
+              onClick={handleCardClick}
+              bin={true}
+              onLeaveSociety={() => handleLeaveSociety(society.id)}
+            />
+          </motion.div>
+        ))}
+      </motion.div>
+      <AnimatePresence>
+        {expandedSociety && (
+          <motion.div
+            className="fixed inset-0 flex items-center justify-center z-50"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <motion.div
+              className="absolute inset-0 bg-black opacity-50"
+              onClick={handleCloseExpandedCard}
+            />
+            <ExpandedSocietyCard
+              society={expandedSociety}
+              onClose={handleCloseExpandedCard}
+              onLeaveSociety={handleLeaveSociety}
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default HomeSocietiesList;
