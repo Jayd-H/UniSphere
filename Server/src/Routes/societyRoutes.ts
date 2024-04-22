66fc29d62002720cbcfa315b9a53359696b8e5a6
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-04-22 02:59:44 +0100

    feat: add route for getting 5 random societies

--- Server/src/Routes/societyRoutes.ts
+++ Server/src/Routes/societyRoutes.ts
@@ -1,14 +1,24 @@
 import { Router } from 'express';
 import { authenticateToken } from '../Middleware/authMiddleware';
-import { getUserSocieties, getAllSocieties, getSpecificSociety, getSocietyPosts, joinSociety, leaveSociety, getSocietyMemberCount } from '../Controllers/societyController';
+import {
+  getUserSocieties,
+  getAllSocieties,
+  getSpecificSociety,
+  getSocietyPosts,
+  joinSociety,
+  leaveSociety,
+  getSocietyMemberCount,
+  getRecommendedSocieties,
+} from '../Controllers/societyController';
 
 const router = Router();
 
 router.get('/all', getAllSocieties);
 router.get('/user', authenticateToken, getUserSocieties);
 router.get('/One', getSpecificSociety);
-router.get('/:societyId/posts', authenticateToken, getSocietyPosts);
+router.get('/recommended', authenticateToken, getRecommendedSocieties);
 
+router.get('/:societyId/posts', authenticateToken, getSocietyPosts);
 router.post('/:societyId/join', authenticateToken, joinSociety);
 router.delete('/:societyId/leave', authenticateToken, leaveSociety);
 router.get('/:societyId/members/count', getSocietyMemberCount);
