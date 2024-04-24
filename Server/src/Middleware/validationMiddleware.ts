d91e315ef4dd7a56f69e9a6f8e26151308463a29
Petra <46404288+BallerHat@users.noreply.github.com>
2024-04-24 11:03:21 +0100

    Allow special characters in validation Closes #153

--- Server/src/Middleware/validationMiddleware.ts
+++ Server/src/Middleware/validationMiddleware.ts
@@ -5,7 +5,7 @@ export const usernameValidator = [
     body('userName')
         .notEmpty().withMessage("Username cannot be empty")
         .isLength({min: 5, max: 32}).withMessage("Username must be between 5 and 32 characters")
-        .isAlphanumeric().withMessage("Username must be alphanumeric")
+        .isAlphanumeric('en-GB', {ignore: "-_."}).withMessage("Username must be alphanumeric")
         .escape()
 ]
 
@@ -21,7 +21,7 @@ export const displayNameValidator = [
     body('displayName')
         .notEmpty().withMessage("Display name cannot be empty")
         .isLength({min: 5, max: 64}).withMessage("Display name must be between 5 and 64 characters")
-        .isAlphanumeric().withMessage("Display name must be alphanumeric")
+        .isAlphanumeric('en-GB', {ignore: " "}).withMessage("Display name must be alphanumeric")
         .escape()
 ]
 
@@ -43,13 +43,13 @@ export const eventValidation = [
     body('eventType')
         .notEmpty().withMessage("Event post must have a type")
         .isLength({min: 1, max: 32}).withMessage("Maximum event type is 32 characters")
-        .isAlphanumeric().withMessage("Event type must be alphanumeric")
+        .isAlphanumeric('en-GB', {ignore: " "}).withMessage("Event type must be alphanumeric")
         .escape(),
     
     body('eventLocation')
         .notEmpty().withMessage("Event post must have a location")
         .isLength({min: 1, max: 64}).withMessage("Maximum event location is 64 characters")
-        .isAlphanumeric().withMessage("Event location must be alpha numeric")
+        .isAlphanumeric('en-GB', {ignore: " "}).withMessage("Event location must be alpha numeric")
         .escape(),
 
     body('eventTime')
