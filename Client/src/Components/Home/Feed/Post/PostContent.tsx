2ef7b2387f5ea0c6abec7604a6822539e96add37
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-04-24 11:48:26 +0100

    fix: content not wrapping with post div

--- Client/src/Components/Home/Feed/Post/PostContent.tsx
+++ Client/src/Components/Home/Feed/Post/PostContent.tsx
@@ -8,7 +8,7 @@ interface PostContentProps {
 const PostContent: React.FC<PostContentProps> = ({ content }) => {
   return (
     <motion.p
-      className="pt-2 pb-6 mr-2 text-dark-grey px-6"
+      className="pt-2 pb-6 mr-2 text-dark-grey px-6 break-words"
       initial={{ opacity: 0, y: 10 }}
       animate={{ opacity: 1, y: 0 }}
     >
